efdfc7e0c3b06ba9e74fec246a85f23ffbd51464
BDUKZZ <lucasaguiarodrigues2019@gmail.com>
2023-01-25 21:17:31 -0300

    codigos do lucas

--- Tarefas/src/Tarefas.web/Controllers/TarefaController.cs
+++ Tarefas/src/Tarefas.web/Controllers/TarefaController.cs
@@ -5,19 +5,33 @@ namespace Tarefas.web.Controllers
 {
     public class TarefaController : Controller 
     {
+        public List<TarefaViewModel> listaDeTarefas { get; set; }
+
+        public TarefaController()
+        {
+          listaDeTarefas = new List<TarefaViewModel>()
+          {
+              new TarefaViewModel(){ Id = 1, Titulo = "Escovar os dentes"},
+              new TarefaViewModel(){Id = 2, "Arrumar a cama"},
+              new TarefaViewModel(){Id = 3, "Por o lixo para fora",Descricao = "somente ás terças"}
+          };
+        }
+
         public IActionResult Create ()
         {
            return View();
         }
-    }
-      public IActionResult Index(){
-        var listaDeTarefas = new List<TarefaViewModel>()
-        {
-            new TarefaViewModel(){Titulo = "Escovar os dentes"},
-            new TarefaViewModel(){Titulo = "Arrumar a cama"},
-            new TarefaViewModel(){Titulo = "Por o lixo para fora",Descricao = "somente ás terças"}
-        }
-
+    
+      public IActionResult Index()
+      {
         return View(listaDeTarefas);
       }
+
+      public IActionResult Details(int id)
+      {
+        var tarefa = listaDeTarefas.Find(m => m.Id == id);
+        return View(tarefa);
+      }
+    
+    }    
 }
--- Tarefas/src/Tarefas.web/Models/TarefaViewModel.cs
+++ Tarefas/src/Tarefas.web/Models/TarefaViewModel.cs
@@ -5,6 +5,7 @@ namespace Tarefas.web.Models;
 public class TarefaViewModel
 
 {
+   public int Id { get; set; }
    [DisplayName("Titulo")]
    public string? Titulo {get;set;}
    [DisplayName("Descricao")]
